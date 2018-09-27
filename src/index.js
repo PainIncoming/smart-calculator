50f75b8b1dfd6bb624cc4b5d078bf267fdbc89c8
PainIncoming <galko3.v@gmail.com>
2018-09-27 07:28:07 +0300

    Implement class SmartCalculator

--- src/index.js
+++ src/index.js
@@ -1,26 +1,64 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this._math = ['**', '*', '/', '-', '+'];    
+    this._data = [initialValue];
   }
 
   add(number) {
-    // your implementation
+    this._data.push('+', number);
+    return this;
   }
-  
+
   subtract(number) {
-    // your implementation
+    this._data.push('-', number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this._data.push('*', number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this._data.push('/', number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this._data.push('**', number);
+    return this;
+  }
+
+  _replace(pos, newValue) {
+    this._data.splice(pos - 1, 3, newValue);
+  }
+  
+  valueOf() {  
+    this._math.forEach(item => {      
+      let search = item !== '**' ? [].indexOf : [].lastIndexOf;
+
+      for (let pos; (pos = search.call(this._data, item)) > -1;) {
+        switch (item) {
+          case '**':
+            this._replace(pos, this._data[pos - 1] ** this._data[pos + 1]);
+            break;
+          case '*':
+            this._replace(pos, this._data[pos - 1] * this._data[pos + 1]);
+            break;
+          case '/':
+            this._replace(pos, this._data[pos - 1] / this._data[pos + 1]);
+            break;
+          case '-':
+            this._replace(pos, this._data[pos - 1] - this._data[pos + 1]);
+            break;
+          case '+':
+            this._replace(pos, this._data[pos - 1] + this._data[pos + 1]);
+            break;
+        }
+      }
+    });
+
+    return this._data[0];
   }
 }
 
